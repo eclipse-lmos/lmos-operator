599fcaa57bd20ff488ce8669d7b54b284dc65f63
Robert Winkler <robwin@t-online.de>
2025-09-05 09:50:44 +0200

    Only Agents which match the subset should be taken into account by the resolver.

--- src/main/kotlin/org/eclipse/lmos/operator/Constants.kt
+++ src/main/kotlin/org/eclipse/lmos/operator/Constants.kt
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
 package org.eclipse.lmos.operator
 
 const val DEPLOYMENT_LABEL_KEY_SUBSET = "subset"
